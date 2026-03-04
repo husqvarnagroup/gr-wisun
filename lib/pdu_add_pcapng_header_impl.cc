98d902e4689ac46b7293591c7e967affc7479cca
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2026-03-04 11:59:29 +0100

    improve code readability for pdu_add_pcap{,ng}_header blocks

--- lib/pdu_add_pcapng_header_impl.cc
+++ lib/pdu_add_pcapng_header_impl.cc
@@ -16,6 +16,9 @@
 #include <gnuradio/pdu.h>
 #include <gnuradio/io_signature.h>
 
+#define WTAP_ENCAP_IEEE802_15_4_NOFCS (230)
+#define WTAP_ENCAP_IEEE802_15_4_TAP (283)
+
 namespace gr {
 namespace wisun {
 
@@ -23,10 +26,10 @@ static const std::string if_name = "GNU Radio Wi-SUN Sniffer";
 static size_t if_name_padding = -if_name.length() % 4;
 static size_t idb_options_length = 4 + if_name.length() + if_name_padding + 4;
 
-static size_t shb_length = 28;
-static size_t idb_length = 20 + idb_options_length;
-static size_t epb_base_length = 28; /* length of EPB header at beginning of EPB */
-static size_t epb_full_length = epb_base_length + 4; /* full length of EPB including 'block total length' at end */
+static size_t shb_length = sizeof(struct pcapng_section_header_block_no_options);
+static size_t idb_length = sizeof(struct pcapng_interface_description_block_base) + idb_options_length + 4;
+static size_t epb_base_length = sizeof(struct pcapng_enhanced_packet_block_base); /* length of EPB header at beginning of EPB */
+static size_t epb_header_full_length = epb_base_length + 4; /* full length of EPB including 'block total length' at end */
 
 
 pdu_add_pcapng_header::sptr
@@ -89,39 +92,18 @@ void pdu_add_pcapng_header_impl::handle_msg(pmt::pmt_t msg)
         uint8_t* buf = pmt::u8vector_writable_elements(shb_vect, shb_length);
 
         /* add section header block */
+        struct pcapng_section_header_block_no_options shb;
 
-        /*
-         *
-         *                         1                   2                   3
-         *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  0 |                   Block Type = 0x0A0D0D0A                     |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  4 |                      Block Total Length                       |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  8 |                      Byte-Order Magic                         |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 12 |          Major Version        |         Minor Version         |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 16 |                                                               |
-         *    |                          Section Length                       |
-         *    |                                                               |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 24 /                                                               /
-         *    /                      Options (variable)                       /
-         *    /                                                               /
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *    |                      Block Total Length                       |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         */
+        shb.block_type             = 0x0a0d0d0a;
+        shb.block_total_length     = shb_length;
+        shb.byte_order_magic       = 0x1a2b3c4d;
+        shb.major_version          = 1;
+        shb.minor_version          = 0;
+        shb.section_length         = 0xffffffffffffffff;
+        shb.block_total_length_dup = shb_length;
 
-        *(uint32_t *)&buf[0] = (uint32_t)0x0a0d0d0a; /* block type */
-        *(uint32_t *)&buf[4] = (uint32_t)shb_length; /* block total length */
-        *(uint32_t *)&buf[8] = (uint32_t)0x1a2b3c4d; /* byte order magic */
-        *(uint16_t *)&buf[12] = (uint16_t)1; /* major version */
-        *(uint16_t *)&buf[14] = (uint16_t)0; /* minor version */
-        *(uint64_t *)&buf[16] = (uint64_t)0xffffffffffffffff; /* section length */
-        *(uint32_t *)&buf[24] = (uint32_t)shb_length; /* block total length */
+        memcpy(buf, &shb, sizeof(shb));
+
         pmt::pmt_t shb_msg = pmt::cons(pmt::get_PMT_NIL(), shb_vect);
         message_port_pub(msgport_names::pdus(), shb_msg);
 
@@ -134,35 +116,15 @@ void pdu_add_pcapng_header_impl::handle_msg(pmt::pmt_t msg)
         uint8_t* buf = pmt::u8vector_writable_elements(idb_vect, idb_length);
 
         /* add interface description block */
+        struct pcapng_interface_description_block_base idb_base;
+        uint16_t dlt = d_use_tap_dlt ? WTAP_ENCAP_IEEE802_15_4_TAP : WTAP_ENCAP_IEEE802_15_4_NOFCS;
 
-        /*
-         *                         1                   2                   3
-         *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  0 |                    Block Type = 0x00000001                    |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  4 |                      Block Total Length                       |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  8 |           LinkType            |           Reserved            |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 12 |                            SnapLen                            |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 16 /                                                               /
-         *    /                      Options (variable)                       /
-         *    /                                                               /
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *    |                      Block Total Length                       |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         */
-        *(uint32_t *)&buf[0] = (uint32_t)0x00000001; /* block type */
-        *(uint32_t *)&buf[4] = (uint32_t)idb_length; /* block total length */
-        if (d_use_tap_dlt) {
-            *(uint16_t *)&buf[8] = (uint16_t)283; /* link type: 283 (WTAP_ENCAP_IEEE802_15_4_TAP) */
-        } else {
-            *(uint16_t *)&buf[8] = (uint16_t)230; /* link type: 230 (WTAP_ENCAP_IEEE802_15_4_NOFCS) */
-        }
-        *(uint16_t *)&buf[10] = (uint16_t)0; /* reserved */
-        *(uint32_t *)&buf[12] = (uint32_t)0; /* snap length (0 = no limit) */
+        idb_base.block_type         = 0x00000001;
+        idb_base.block_total_length = idb_length;
+        idb_base.link_type          = dlt;
+        idb_base.reserved           = 0;
+        idb_base.snap_length        = 0;
+        memcpy(buf, &idb_base, sizeof(idb_base));
 
         /* options: interface name */
         *(uint16_t *)&buf[16] = (uint16_t)2; /* option code */
@@ -179,6 +141,7 @@ void pdu_add_pcapng_header_impl::handle_msg(pmt::pmt_t msg)
         *(uint16_t *)&buf[16 + idb_options_length - 2] = 0; /* option length */
 
         *(uint32_t *)&buf[16 + idb_options_length] = (uint32_t)idb_length; /* block total length */
+
         pmt::pmt_t idb_msg = pmt::cons(pmt::get_PMT_NIL(), idb_vect);
         message_port_pub(msgport_names::pdus(), idb_msg);
 
@@ -210,80 +173,68 @@ void pdu_add_pcapng_header_impl::handle_msg(pmt::pmt_t msg)
             d_logger->debug("packet RSSI: {:f} dBm", rssi);
         }
     }
-    size_t output_length = epb_full_length + tap_header_length + payload_length + padding;
+    size_t output_length = epb_header_full_length + tap_header_length + payload_length + padding;
     pmt::pmt_t out_vect = pmt::make_u8vector(output_length, 0);
     uint8_t* out = pmt::u8vector_writable_elements(out_vect, output_length);
 
     /* add enhanced packet block (EPB) */
-
-    /*
-     *                         1                   2                   3
-     *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  0 |                    Block Type = 0x00000006                    |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  4 |                      Block Total Length                       |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  8 |                         Interface ID                          |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 12 |                        Timestamp (High)                       |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 16 |                        Timestamp (Low)                        |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 20 |                    Captured Packet Length                     |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 24 |                    Original Packet Length                     |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 28 /                                                               /
-     *    /                          Packet Data                          /
-     *    /              variable length, padded to 32 bits               /
-     *    /                                                               /
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *    /                                                               /
-     *    /                      Options (variable)                       /
-     *    /                                                               /
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *    |                      Block Total Length                       |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     */
+    struct pcapng_enhanced_packet_block_base epb_base;
 
     clock_gettime(CLOCK_REALTIME, &curtime);
     timestamp = curtime.tv_sec * 1000000 + curtime.tv_nsec / 1000;
 
-    *(uint32_t *)&out[0] = (uint32_t)0x6; /* block type */
-    *(uint32_t *)&out[4] = (uint32_t)output_length; /* block total length */
-    *(uint32_t *)&out[8] = (uint32_t)0; /* interface id */
-    *(uint32_t *)&out[12] = (uint32_t)(timestamp >> 32); /* timestamp (high) */
-    *(uint32_t *)&out[16] = (uint32_t)(timestamp & 0xffffffff); /* timestamp (low) */
-    *(uint32_t *)&out[20] = (uint32_t)payload_length + tap_header_length; /* captured packet length */
-    *(uint32_t *)&out[24] = (uint32_t)payload_length + tap_header_length; /* original packet length */
+    epb_base.block_type             = 0x6;
+    epb_base.block_total_length     = output_length;
+    epb_base.interface_id           = 0;
+    epb_base.timestamp_high         = (timestamp >> 32);
+    epb_base.timestamp_low          = (timestamp & 0xffffffff);
+    epb_base.captured_packet_length = payload_length + tap_header_length;
+    epb_base.original_packet_length = payload_length + tap_header_length;
+    memcpy(out, &epb_base, sizeof(epb_base));
+
     *(uint32_t *)&out[output_length - 4] = (uint32_t)output_length; /* block total length */
 
     /* add TAP header (if TAP DLT enabled) */
-    uint16_t idx = 28;
+    uint16_t idx = sizeof(epb_base);
     if (d_use_tap_dlt) {
+        struct ieee802_15_4_tap_dlt_header tap_hdr;
         /*
          * Note: in contrast to the PCAPNG specification (which states that each section
          * uses the endianness of the capturing machine), the IEEE 802.15.4 TAP link type
          * specification by Exegin states that all data fields are encoded in
          * little-endian byte order. This needs we need to take care of endianness.
          */
-        *(uint8_t *)&out[idx] = (uint8_t)0; /* version */
-        *(uint8_t *)&out[idx + 1] = (uint8_t)0; /* padding */
-        *(uint16_t *)&out[idx + 2] = htole16(tap_header_length); /* length */
-        idx += 4;
+        tap_hdr.version           = 0;
+        tap_hdr.padding           = 0;
+        tap_hdr.tap_header_length = htole16(tap_header_length);
+        memcpy(&out[idx], &tap_hdr, sizeof(tap_hdr));
+        idx += sizeof(tap_hdr);
 
         /* RSSI */
         if (has_rssi) {
-            *(uint16_t *)&out[idx] = htole16(1); /* type = RSS */
-            *(uint16_t *)&out[idx + 2] = htole16(4); /* length of data: 4 bytes */
-            /* the following has not been tested, but should (possibly) work to convert float to LE: */
+            struct ieee802_15_4_tap_dlt_header_rss rss_hdr;
+            rss_hdr.type   = htole16(1); /* type 1 = RSS */
+            rss_hdr.length = htole16(4); /* length of data: 4 bytes */
+
+            /*
+             * Note: on a little-endian machine, we could just use
+             *
+             * rss_hdr.value = rssi;
+             *
+             * The following has not been tested, but should (possibly) work to convert
+             * float to LE:
+             */
             uint32_t rssi_hack;
             memcpy(&rssi_hack, &rssi, 4);
-            *(uint32_t *)&out[idx + 4] = htole32(rssi_hack);
-            idx += 8;
+            rssi_hack = htole32(rssi_hack);
+            memcpy(&rss_hdr.value, &rssi_hack, 4);
+
+            memcpy(&out[idx], &rss_hdr, sizeof(rss_hdr));
+            idx += sizeof(rss_hdr);
         }
     }
+
+    assert(idx == epb_base_length + tap_header_length);
 
     /*
      * add data
--- lib/pdu_add_pcapng_header_impl.h
+++ lib/pdu_add_pcapng_header_impl.h
@@ -13,6 +13,153 @@
 namespace gr {
 namespace wisun {
 
+/*
+ * PCAPNG: Section Header Block (SHB)
+ */
+
+#pragma pack(push, 1)
+/*
+ *                         1                   2                   3
+ *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  0 |                   Block Type = 0x0A0D0D0A                     |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  4 |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  8 |                      Byte-Order Magic                         |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 12 |          Major Version        |         Minor Version         |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 16 |                                                               |
+ *    |                          Section Length                       |
+ *    |                                                               |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 24 /                                                               /
+ *    /                      Options (variable)                       /
+ *    /                                                               /
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *    |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct pcapng_section_header_block_no_options {
+    uint32_t block_type;
+    uint32_t block_total_length;
+    uint32_t byte_order_magic;
+    uint16_t major_version;
+    uint16_t minor_version;
+    uint64_t section_length;
+    uint32_t block_total_length_dup;
+};
+static_assert(sizeof(struct pcapng_section_header_block_no_options) == 28);
+#pragma pack(pop)
+
+/*
+ * PCAP: Interface Description Block (IDB) (inital part only)
+ */
+
+#pragma pack(push, 1)
+/*
+ *                         1                   2                   3
+ *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  0 |                    Block Type = 0x00000001                    |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  4 |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  8 |           LinkType            |           Reserved            |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 12 |                            SnapLen                            |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 16 /                                                               /
+ *    /                      Options (variable)                       /
+ *    /                                                               /
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *    |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct pcapng_interface_description_block_base {
+    uint32_t block_type;
+    uint32_t block_total_length;
+    uint16_t link_type;
+    uint16_t reserved;
+    uint32_t snap_length;
+};
+static_assert(sizeof(struct pcapng_interface_description_block_base) == 16);
+#pragma pack(pop)
+
+/*
+ * PCAPNG: Enhanced Packet Block (EPB)  (initial part only)
+ */
+
+#pragma pack(push, 1)
+/*
+ *                         1                   2                   3
+ *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  0 |                    Block Type = 0x00000006                    |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  4 |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  8 |                         Interface ID                          |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 12 |                        Timestamp (High)                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 16 |                        Timestamp (Low)                        |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 20 |                    Captured Packet Length                     |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 24 |                    Original Packet Length                     |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 28 /                                                               /
+ *    /                          Packet Data                          /
+ *    /              variable length, padded to 32 bits               /
+ *    /                                                               /
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *    /                                                               /
+ *    /                      Options (variable)                       /
+ *    /                                                               /
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *    |                      Block Total Length                       |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct pcapng_enhanced_packet_block_base {
+    uint32_t block_type;
+    uint32_t block_total_length;
+    uint32_t interface_id;
+    uint32_t timestamp_high;
+    uint32_t timestamp_low;
+    uint32_t captured_packet_length;
+    uint32_t original_packet_length;
+};
+static_assert(sizeof(struct pcapng_enhanced_packet_block_base) == 28);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header {
+    uint8_t version;
+    uint8_t padding;
+    uint16_t tap_header_length;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header) == 4);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Received Signal Strength (RSS)
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_rss {
+    uint16_t type;
+    uint16_t length;
+    float value;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_rss) == 8);
+#pragma pack(pop)
+
 class pdu_add_pcapng_header_impl : public pdu_add_pcapng_header
 {
 private:
