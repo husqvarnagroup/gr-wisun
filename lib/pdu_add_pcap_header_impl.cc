fc7d3152a915882eff3fdcc0b501bea16011ab9d
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2026-03-04 11:59:29 +0100

    improve code readability for pdu_add_pcap{,ng}_header blocks

--- lib/pdu_add_pcap_header_impl.cc
+++ lib/pdu_add_pcap_header_impl.cc
@@ -7,17 +7,23 @@
  * Information about the PCAP header is available at:
  * - https://datatracker.ietf.org/doc/draft-ietf-opsawg-pcap/
  * - https://wiki.wireshark.org/Development/LibpcapFileFormat#Record_.28Packet.29_Header
+ *
+ * Note: The PCAP format specifies that all file and packet record header fields are
+ * always written in the characteristics of the machine writing the file. This means we do
+ * not need to worry about endianness (the magic number is used to handle endianness when
+ * reading the file).
  */
 
 #include "pdu_add_pcap_header_impl.h"
 #include <gnuradio/pdu.h>
 #include <gnuradio/io_signature.h>
 
+#define WTAP_ENCAP_IEEE802_15_4_NOFCS (230)
+
 namespace gr {
 namespace wisun {
 
-static size_t pcap_global_header_length = 24;
-static const size_t pcap_packet_record_header_length = 16;
+static size_t pcap_file_header_length = sizeof(struct pcap_file_header);
 
 pdu_add_pcap_header::sptr pdu_add_pcap_header::make(const bool prepend_global_pcap_header)
 {
@@ -51,52 +57,32 @@ void pdu_add_pcap_header_impl::handle_msg(pmt::pmt_t msg)
 {
     pmt::pmt_t msg_meta;
     pmt::pmt_t msg_vect;
-    size_t input_length;
+    size_t input_length, payload_length;
     struct timespec curtime;
+    struct pcap_packet_record_header packet_hdr;
 
     if (!pmt::is_pair(msg))
         throw std::runtime_error("received a malformed PDU message");
 
-    /*
-     * Note: The PCAP format specifies that all file and packet record header fields are
-     * always written in the characteristics of the machine writing the file. This means
-     * we do not need to worry about endianness (the magic number is used to handle
-     * endianness when reading the file).
-     */
-
     /* if enabled, create PDU with global PCAP header initially */
     if (d_prepend_global_pcap_header && !d_global_pcap_header_done) {
-        pmt::pmt_t global_header_vect = pmt::make_u8vector(pcap_global_header_length, 0);
+        struct pcap_file_header fhdr;
+
+        pmt::pmt_t global_header_vect = pmt::make_u8vector(pcap_file_header_length, 0);
         uint8_t* buf = pmt::u8vector_writable_elements(global_header_vect,
-                                                       pcap_global_header_length);
+                                                       pcap_file_header_length);
 
         /* add global PCAP header */
+        fhdr.magic_number  = 0xA1B2C3D4; /* indicates using seconds & microseconds */
+        fhdr.major_version = 2;
+        fhdr.minor_version = 0;
+        fhdr.reserved1     = 0;
+        fhdr.reserved2     = 0;
+        fhdr.snap_len      = 0x7ff;
+        fhdr.link_type     = WTAP_ENCAP_IEEE802_15_4_NOFCS;
 
-        /*
-         *                         1                   2                   3
-         *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  0 |                          Magic Number                         |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  4 |         Major Version         |         Minor Version         |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         *  8 |                           Reserved1                           |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 12 |                           Reserved2                           |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 16 |                            SnapLen                            |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         * 20 |               LinkType and additional information             |
-         *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-         */
+        memcpy(buf, &fhdr, sizeof(fhdr));
 
-        *(uint32_t *)&buf[0] = (uint32_t)0xA1B2C3D4; /* magic number; use seconds & microseconds */
-        *(uint16_t *)&buf[4] = (uint16_t)2; /* major version */
-        *(uint16_t *)&buf[6] = (uint16_t)0; /* minor version */
-        *(uint32_t *)&buf[8] = (uint32_t)0; /* reserved 1 */
-        *(uint32_t *)&buf[12] = (uint32_t)0; /* reserved 2 */
-        *(uint32_t *)&buf[16] = (uint32_t)0x7ff; /* snap len */
-        *(uint16_t *)&buf[20] = (uint32_t)230; /* link type: 230 (WTAP_ENCAP_IEEE802_15_4_NOFCS) */
         pmt::pmt_t global_hdr_msg = pmt::cons(pmt::get_PMT_NIL(), global_header_vect);
         message_port_pub(msgport_names::pdus(), global_hdr_msg);
 
@@ -107,40 +93,31 @@ void pdu_add_pcap_header_impl::handle_msg(pmt::pmt_t msg)
     pmt::print(msg_meta);
     msg_vect = pmt::cdr(msg);
     input_length = pmt::blob_length(msg_vect);
+    payload_length = input_length - 4;  /* first 4 bytes (SFD & PHR) are not part of the payload */
     const std::vector<uint8_t> in = pmt::u8vector_elements(msg_vect);
 
     /* create output PDU */
-    size_t output_length = pcap_packet_record_header_length + input_length - 4;
+    size_t output_length = sizeof(struct pcap_packet_record_header) + payload_length;
     pmt::pmt_t out_vect = pmt::make_u8vector(output_length, 0);
     uint8_t* out = pmt::u8vector_writable_elements(out_vect, output_length);
 
     /* add PCAP packet record header */
-
-    /*
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  0 |                      Timestamp (Seconds)                      |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  4 |            Timestamp (Microseconds or nanoseconds)            |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     *  8 |                    Captured Packet Length                     |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     * 12 |                    Original Packet Length                     |
-     *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-     */
-
     clock_gettime(CLOCK_REALTIME, &curtime);
-    *(uint32_t *)&out[0] = (uint32_t)(curtime.tv_sec);
-    *(uint32_t *)&out[4] = (uint32_t)(curtime.tv_nsec / 1000);
-    *(uint32_t *)&out[8] = (uint32_t)input_length - 4;
-    *(uint32_t *)&out[12] = (uint32_t)input_length - 4;
+
+    packet_hdr.timestamp_s            = curtime.tv_sec;
+    packet_hdr.timestamp_us           = curtime.tv_nsec / 1000;
+    packet_hdr.captured_packet_length = payload_length;
+    packet_hdr.original_packet_length = payload_length;
+
+    memcpy(out, &packet_hdr, sizeof(packet_hdr));
 
     /*
      * add data
      *
      * note: we skip the first 4 bytes (SFD & PHR)
      */
-    for (size_t i = 4; i < input_length; i++) {
-        out[pcap_packet_record_header_length + i - 4] = in[i];
+    for (size_t i = 0; i < payload_length; i++) {
+        out[sizeof(struct pcap_packet_record_header) + i] = in[i + 4];
     }
 
     /* create & send msg */
--- lib/pdu_add_pcap_header_impl.h
+++ lib/pdu_add_pcap_header_impl.h
@@ -13,6 +13,65 @@
 namespace gr {
 namespace wisun {
 
+
+/*
+ * PCAP file header
+ */
+
+#pragma pack(push, 1)
+/*                         1                   2                   3
+ *     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  0 |                          Magic Number                         |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  4 |         Major Version         |         Minor Version         |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  8 |                           Reserved1                           |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 12 |                           Reserved2                           |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 16 |                            SnapLen                            |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 20 |               LinkType and additional information             |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct pcap_file_header {
+    uint32_t magic_number;
+    uint16_t major_version;
+    uint16_t minor_version;
+    uint32_t reserved1;
+    uint32_t reserved2;
+    uint32_t snap_len;
+    uint32_t link_type;
+};
+static_assert(sizeof(struct pcap_file_header) == 24);
+#pragma pack(pop)
+
+/*
+ * PCAP packet record header
+ */
+
+#pragma pack(push, 1)
+/*
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  0 |                      Timestamp (Seconds)                      |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  4 |            Timestamp (Microseconds or nanoseconds)            |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *  8 |                    Captured Packet Length                     |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 12 |                    Original Packet Length                     |
+ *    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct pcap_packet_record_header {
+    uint32_t timestamp_s;
+    uint32_t timestamp_us;
+    uint32_t captured_packet_length;
+    uint32_t original_packet_length;
+};
+static_assert(sizeof(struct pcap_packet_record_header) == 16);
+#pragma pack(pop)
+
 class pdu_add_pcap_header_impl : public pdu_add_pcap_header
 {
 private:
