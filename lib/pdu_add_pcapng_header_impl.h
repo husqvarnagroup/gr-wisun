188624e517f987018ab535282b3886efad4f8313
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2026-03-27 19:19:22 +0100

    pdu_add_pcapng_header: support more metadata

--- lib/pdu_add_pcapng_header_impl.h
+++ lib/pdu_add_pcapng_header_impl.h
@@ -148,6 +148,37 @@ static_assert(sizeof(struct ieee802_15_4_tap_dlt_header) == 4);
 #pragma pack(pop)
 
 /*
+ * IEEE 802.15.4 TAP DLT TLV types
+ */
+enum ieee802_15_4_tap_dlt_tlv_types {
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_FCS_TYPE                 = 0,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_RSS                      = 1,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_BIT_RATE                 = 2,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_CHANNEL_ASSIGNMENT       = 3,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_SUN_PHY_INFORMATION      = 4,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_START_OF_FRAME_TIMESTAMP = 5,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_END_OF_FRAME_TIMESTAMP   = 6,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_ASN                      = 7,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_START_OF_SLOT_TIMESTAMP  = 8,
+    IEEE802_15_4_TAP_DLT_TLV_TYPE_TIMESLOT_LENGTH          = 9,
+};
+
+/*
+ * IEEE 802.15.4 TAP DLT header: FCS Type
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_fcs_type {
+    uint16_t type;
+    uint16_t length;
+    uint8_t fcs_type;
+    uint8_t padding1;
+    uint16_t padding2;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_fcs_type) == 8);
+#pragma pack(pop)
+
+/*
  * IEEE 802.15.4 TAP DLT header: Received Signal Strength (RSS)
  */
 
@@ -158,6 +189,115 @@ struct ieee802_15_4_tap_dlt_header_rss {
     float value;
 };
 static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_rss) == 8);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Bit Rate
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_bit_rate {
+    uint16_t type;
+    uint16_t length;
+    uint32_t bit_rate;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_bit_rate) == 8);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Channel Assignment
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_channel_assignment {
+    uint16_t type;
+    uint16_t length;
+    uint16_t channel_number;
+    uint8_t channel_page;
+    uint8_t padding;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_channel_assignment) == 8);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: SUN PHY Information
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_sun_phy_information {
+    uint16_t type;
+    uint16_t length;
+    uint8_t phy_band;
+    uint8_t phy_type;
+    uint8_t phy_mode;
+    uint8_t padding;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_sun_phy_information) == 8);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Start of Frame Timestamp
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_start_of_frame_timestamp {
+    uint16_t type;
+    uint16_t length;
+    uint64_t nanoseconds;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_start_of_frame_timestamp) == 12);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: End of Frame Timestamp
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_end_of_frame_timestamp {
+    uint16_t type;
+    uint16_t length;
+    uint64_t nanoseconds;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_end_of_frame_timestamp) == 12);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Absolute Slot Number (ASN)
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_absolute_slot_number {
+    uint16_t type;
+    uint16_t length;
+    uint64_t asn;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_absolute_slot_number) == 12);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Start of Slot Timestamp
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_start_of_slot_timestamp {
+    uint16_t type;
+    uint16_t length;
+    uint64_t nanoseconds;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_start_of_slot_timestamp) == 12);
+#pragma pack(pop)
+
+/*
+ * IEEE 802.15.4 TAP DLT header: Start of Slot Timestamp
+ */
+
+#pragma pack(push, 1)
+struct ieee802_15_4_tap_dlt_header_timeslot_length {
+    uint16_t type;
+    uint16_t length;
+    uint32_t microseconds;
+};
+static_assert(sizeof(struct ieee802_15_4_tap_dlt_header_timeslot_length) == 8);
 #pragma pack(pop)
 
 class pdu_add_pcapng_header_impl : public pdu_add_pcapng_header
