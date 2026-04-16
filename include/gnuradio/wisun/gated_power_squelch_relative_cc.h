2927c389d487cb4e6985875cb59c8c724f61b26f
Andreas Müller <andreas.mueller@husqvarnagroup.com>
2026-04-16 14:50:10 +0200

    add implementation for gated_power_squelch_relative_cc block

--- include/gnuradio/wisun/gated_power_squelch_relative_cc.h
+++ include/gnuradio/wisun/gated_power_squelch_relative_cc.h
@@ -15,7 +15,8 @@ namespace gr {
 namespace wisun {
 
 /*!
- * \brief <+description of block+>
+ * \brief Gated variant of power_squelch_relative_cc block (truncates samples after power
+ * drops below threshold).
  * \ingroup wisun
  *
  */
@@ -27,6 +28,13 @@ public:
     /*!
      * \brief Return a shared_ptr to a new instance of
      * wisun::gated_power_squelch_relative_cc.
+     * \param relative_threshold Relative threshold for signal level above noise floor in
+     * dB (i.e. this is the threshold to *unsquelch* and should be a positive number).
+     * \param alpha Gain of averaging filter.
+     * \param trailing_samples Number of trailing samples after signal drops below
+     * threshold. May be needed to make sure downstream blocks have enough samples to
+     * process the packet (otherwise, packets may only be processed once the next packet
+     * arrives).
      *
      * To avoid accidental use of raw pointers, wisun::gated_power_squelch_relative_cc's
      * constructor is in a private implementation
--- python/wisun/qa_gated_power_squelch_relative_cc.py
+++ python/wisun/qa_gated_power_squelch_relative_cc.py
@@ -7,7 +7,7 @@
 #
 
 from gnuradio import gr, gr_unittest
-# from gnuradio import blocks
+from gnuradio import blocks
 try:
     from gnuradio.wisun import gated_power_squelch_relative_cc
 except ImportError:
@@ -17,22 +17,53 @@ except ImportError:
     sys.path.append(os.path.join(dirname, "bindings"))
     from gnuradio.wisun import gated_power_squelch_relative_cc
 
+
 class qa_gated_power_squelch_relative_cc(gr_unittest.TestCase):
+    """Unit tests for gated_power_squelch_relative_cc block."""
 
     def setUp(self):
+        """Set up top block."""
         self.tb = gr.top_block()
 
     def tearDown(self):
+        """Teardown."""
         self.tb = None
 
-    def test_instance(self):
-        # FIXME: Test will fail until you pass sensible arguments to the constructor
-        instance = gated_power_squelch_relative_cc()
+    def test_001_basic(self):
+        """Test basic block funciton."""
+        src_data = (
+            1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9, 1e-9
+        )
 
-    def test_001_descriptive_test_name(self):
+        expected_result = (
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 0, 0
+        )
+
         # set up fg
+        src = blocks.vector_source_c(src_data)
+        blk = gated_power_squelch_relative_cc(30, 1, 3)
+        dst = blocks.vector_sink_c()
+        self.tb.connect(src, blk)
+        self.tb.connect(blk, dst)
         self.tb.run()
         # check data
+        result_data = dst.data()
+        self.assertEqual(len(result_data), len(expected_result))
+        self.assertComplexTuplesAlmostEqual(result_data, expected_result, places=10)
+        # check tags
+        self.assertEqual(len(dst.tags()), 2)
+        tags = dst.tags()
+        tag_sob = tags[0]
+        self.assertEqual(str(tag_sob.key), "squelch_sob")
+        self.assertEqual(tag_sob.offset, 0)
+        self.assertAlmostEqual(float(str(tag_sob.value)), 1)
+        tag_eob = tags[1]
+        self.assertEqual(str(tag_eob.key), "squelch_eob")
+        self.assertEqual(tag_eob.offset, 9)
+        self.assertAlmostEqual(float(str(tag_eob.value)), 0)
 
 
 if __name__ == '__main__':
